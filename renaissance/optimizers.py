7d77881b7a9ff17fcaa0e714d7cf9f3e0956e1e7
Mehmet Berk Şahin <68302641+Sahin-hub@users.noreply.github.com>
2024-05-04 18:37:02 -0400

    Add files via upload

---
+++ renaissance/optimizers.py
@@ -0,0 +1,279 @@
+import torch
+import torch.nn.functional as F
+
+class Adam(object):
+    def __init__(self, betas, weight_decay=0):
+        self.b1, self.b2 = betas 
+        self.weight_decay = weight_decay
+        self.m = []
+        self.v = []
+        self.t = 0
+
+    def get_update(self, update, params, idx):
+        # initialize moment terms with 0 tensors if not done 
+        if self.t == 1:
+            self.m.append(torch.zeros_like(update))
+            self.v.append(torch.zeros_like(update))
+            idx = -1
+        # get the recent moment terms for the given layer
+        m = self.m[idx]
+        v = self.v[idx]
+
+        if self.weight_decay != 0:
+            update.add_(self.weight_decay * params)
+        # first moment update 
+        m = self.b1 * m + (1 - self.b1) * update 
+        # second moment update 
+        v = self.b2 * v + (1 - self.b2) * torch.pow(update, 2)
+        m_unbias = m / (1 - self.b1 ** self.t)
+        v_unbias = v / (1 - self.b2 ** self.t)
+        new_update = torch.div(m_unbias, torch.sqrt(v_unbias) + 1e-6)
+        # update the moments 
+        self.m[idx] = m
+        self.v[idx] = v
+        return new_update
+
+        
+class WeightCalculator(torch.optim.Optimizer):
+    def __init__(self, params, alg_name, topk_ratio=None):
+        defaults = dict(lr=0, reg=0)
+        super(WeightCalculator, self).__init__(params, defaults)
+        self.params = params 
+        self.alg_name = alg_name 
+        if 'loss' in alg_name:
+            self.sample_losses = None
+        self.topk_ratio = topk_ratio
+    
+    def calc_weights(self):
+        if 'loss' in self.alg_name:
+            # only sample with the worst loss used 
+            if self.alg_name == 'maxloss_hard':
+                idx = torch.argmax(self.sample_losses)
+                mask = torch.zeros_like(self.sample_losses)
+                mask[idx] = 1
+                weights = mask 
+            # only sample with the best loss used
+            elif self.alg_name == 'minloss_hard':
+                idx = torch.argmin(self.sample_losses)
+                mask = torch.zeros_like(self.sample_losses)
+                mask[idx] = 1
+                weights = mask
+            # using loss in exponential 
+            elif self.alg_name == 'maxloss_soft':
+                weights = F.softmax(self.sample_losses / self.reg, dim=0)
+            # using negative loss in exponential
+            elif self.alg_name == 'minloss_soft':
+                weights = F.softmax(-self.sample_losses / self.reg, dim=0)
+            # max loss topk
+            elif self.alg_name == 'maxloss_topk':
+                k=int(self.topk_ratio * len(self.sample_losses))
+                _, indices = torch.topk(self.sample_losses, k=k)
+                mask = torch.zeros_like(self.sample_losses)
+                mask[indices] = 1/k
+                weights = mask
+            elif self.alg_name == 'minloss_topk':
+                k = int(self.topk_ratio * len(self.sample_losses))
+                _, indices = torch.topk(self.sample_losses, k=k, largest=False)
+                mask = torch.zeros_like(self.sample_losses)
+                mask[indices] = 1/k
+                weights = mask 
+            else:
+                raise ValueError("Invalid algorithm name!")
+        else:
+            total_weight = 0
+            layer_count = 0
+            for group in self.param_groups:
+                for p in group["params"]:
+                    if type(p.grad_sample) is list:
+                        p.grad_sample = p.grad_sample[-1]
+
+                    layer_count += 1
+                    # only sample with the largest/smallest gradient norm 
+                    if 'norm' in self.alg_name:
+                        total_weight += torch.sum(torch.flatten(p.grad_sample, 1) ** 2, dim=1)
+                    # only sample with the largest/negative positive correlation 
+                    elif 'corr' in self.alg_name:
+                        total_weight += torch.sum(torch.flatten(p.grad) * torch.flatten(p.grad_sample, 1), dim=1)
+                    # using gradient norm in exponential
+                    elif self.alg_name == 'maxnorm_soft':
+                        total_weight += torch.flatten(p.grad_sample, 1)**2 / self.reg
+                    # using negative gradient norm in exponential
+                    elif self.alg_name == 'minnorm_soft':
+                        total_weight += -torch.flatten(p.grad_sample, 1)**2 / self.regression  
+
+            if self.alg_name in ['maxnorm_hard', 'maxcorr_hard']:
+                idx = torch.argmax(total_weight)
+                mask = torch.zeros_like(total_weight)
+                mask[idx] = 1 
+                weights = mask
+                #weights = torch.tile(mask[None,:], (layer_count, 1))
+            # choose maximum top-k elements
+            elif self.alg_name in ['maxcorr_topk', 'maxnorm_topk']:
+                k = int(self.topk_ratio * len(total_weight))
+                _, indices = torch.topk(total_weight, k=k)
+                mask = torch.zeros_like(total_weight)
+                mask[indices] = 1/k
+                weights = mask
+                #weights = torch.tile(mask[None,:], (layer_count, 1))
+            elif self.alg_name in ['minnorm_hard', 'mincorr_hard']:
+                idx = torch.argmin(total_weight)
+                mask = torch.zeros_like(total_weight)
+                mask[idx] = 1 
+                weights = mask
+                #weights = torch.tile(mask[None,:], (layer_count, 1))
+            # choose minimum top-k elements
+            elif self.alg_name in ['mincorr_topk', 'minnorm_topk']:
+                k = int(self.topk_ratio * len(total_weight))
+                _, indices = torch.topk(total_weight, k=k, largest=False)
+                mask = torch.zeros_like(total_weight)
+                mask[indices] = 1/k 
+                weights = mask
+            elif self.alg_name in ['maxcorr_soft', 'mincorr_soft', 'maxnorm_soft', 'minnorm_soft', 'poscorr_soft']:
+                if self.alg_name == 'poscorr_soft':
+                    total_weight = torch.where(total_weight > 0, total_weight, 0)
+                weights = F.softmax(total_weight, dim=0)
+
+        return weights
+
+class KLSGD(torch.optim.Optimizer):
+    def __init__(self, params, robust=False, lr=1e-3, reg=0.5, adam=False, alg_no=-1, topk=None):
+        if lr < 0.0:
+            raise ValueError(f"Invalid learning rate: {lr}")
+        if reg < 0.0:
+            raise ValueError(f"Invalid regularization weight: {reg}")
+
+        # Dictionary to store gradients for each parameter
+        defaults = dict(lr=lr, reg=reg)
+        self.lr = lr
+        self.reg = reg
+        self.robust = robust
+        self.alg_no = alg_no
+        self.topk_ratio = topk
+        super(KLSGD, self).__init__(params, defaults)
+        self.dir = 1 if self.robust else -1
+        self.adam = Adam(betas=(.9, .99)) if adam else None 
+        if alg_no in (4, 5, 6, 8, 18, 19):
+            self.sample_losses = None
+
+    def calc_weights(self):
+        weights = []
+
+        if self.alg_no in (10, 11, 12, 13, 14, 15, 16, 17, 18, 19):
+            total_weight = 0 
+
+        layer_count = 0
+        for group in self.param_groups:
+            for p in group["params"]:
+                if type(p.grad_sample) is list:
+                    p.grad_sample = p.grad_sample[-1]
+
+                layer_count += 1
+                # only sample with the largest/smallest gradient norm 
+                if self.alg_no in (10, 11, 16, 17):
+                    total_weight += torch.sum(torch.flatten(p.grad_sample, 1) ** 2, dim=1)
+                # only sample with the largest/negative positive correlation 
+                elif self.alg_no in (12, 13, 14, 15):
+                    total_weight += torch.sum(torch.flatten(p.grad) * torch.flatten(p.grad_sample, 1), dim=1)
+                # only sample with the worst loss used 
+                if self.alg_no == 4:
+                    idx = torch.argmax(self.sample_losses)
+                    mask = torch.zeros_like(self.sample_losses)
+                    mask[idx] = 1
+                    grad_weights = mask
+                # only sample with the best loss used
+                elif self.alg_no == 5:
+                    idx = torch.argmin(self.sample_losses)
+                    mask = torch.zeros_like(self.sample_losses)
+                    mask[idx] = 1
+                    grad_weights = mask
+                # using loss in exponential 
+                elif self.alg_no == 6:
+                    energy = self.sample_losses / self.reg
+                    grad_weights = F.softmax(energy, dim=0)
+                # using gradient norm in exponential
+                elif self.alg_no == 7:
+                    energy = torch.norm(torch.flatten(p.grad_sample, 1), p=2, dim=1)**2 / self.reg
+                    grad_weights = F.softmax(energy, dim=0)
+                # using negative loss in exponential
+                elif self.alg_no == 8:
+                    energy = -self.sample_losses / self.reg
+                    grad_weights = F.softmax(energy, dim=0)
+                # using negative gradient norm in exponential
+                elif self.alg_no == 9:
+                    energy = -torch.norm(torch.flatten(p.grad_sample, 1), p=2, dim=1)**2 / self.reg
+                    grad_weights = F.softmax(energy, dim=0)
+                # max loss topk
+                elif self.alg_no == 18:
+                    k=int(self.topk_ratio * len(self.sample_losses))
+                    _, indices = torch.topk(self.sample_losses, k=k)
+                    mask = torch.zeros_like(self.sample_losses)
+                    mask[indices] = 1/k
+                    grad_weights = mask
+                elif self.alg_no == 19:
+                    k = int(self.topk_ratio * len(self.sample_losses))
+                    _, indices = torch.topk(self.sample_losses, k=k, largest=False)
+                    mask = torch.zeros_like(self.sample_losses)
+                    mask[indices] = 1/k
+                    grad_weights = mask
+                else:
+                    # KLSGD algorithm 
+                    dot_product = torch.sum(torch.flatten(p.grad) * torch.flatten(p.grad_sample, 1), dim=1)
+                    energy = self.dir * self.lr * dot_product / self.reg # torch.exp(self.dir * self.lr * dot_product / self.reg)
+                    # print(energy)
+                    grad_weights = F.softmax(energy, dim=0)
+
+                    
+                if self.alg_no not in (10,11,12,13):
+                    weights.append(grad_weights)
+
+        if self.alg_no in (10, 12):
+            idx = torch.argmax(total_weight)
+            mask = torch.zeros_like(total_weight)
+            mask[idx] = 1 
+            weights = torch.tile(mask[None,:], (layer_count, 1))
+        # choose maximum top-k elements
+        elif self.alg_no in (14, 16):
+            k = int(self.topk_ratio * len(total_weight))
+            _, indices = torch.topk(total_weight, k=k)
+            mask = torch.zeros_like(total_weight)
+            mask[indices] = 1/k
+            weights = torch.tile(mask[None,:], (layer_count, 1))
+        elif self.alg_no in (11, 13):
+            idx = torch.argmin(total_weight)
+            mask = torch.zeros_like(total_weight)
+            mask[idx] = 1 
+            weights = torch.tile(mask[None,:], (layer_count, 1))
+        # choose minimum top-k elements
+        elif self.alg_no in (15, 17):
+            k = int(self.topk_ratio * len(total_weight))
+            _, indices = torch.topk(total_weight, k=k, largest=False)
+            mask = torch.zeros_like(total_weight)
+            mask[indices] = 1/k
+            weights = torch.tile(mask[None,:], (layer_count, 1))
+
+        return weights
+
+
+    def step(self, closure=None):
+        if self.adam is not None:
+            self.adam.t += 1
+        # calculate the mean gradient for mini-batch 
+        weights = self.calc_weights()
+        idx = -1
+        for group in self.param_groups:
+            for p in group["params"]:
+                idx += 1
+                weight = weights[idx]
+                update = torch.zeros_like(p.grad)
+                
+                if type(p.grad_sample) is list:
+                    p.grad_sample = p.grad_sample[-1]
+
+                product = weight.view(-1, *([1] * (p.grad_sample.dim() - 1))) * p.grad_sample
+                update.add_(torch.sum(product, axis=0))
+
+                if self.adam is None:
+                    p.data.add_(-self.lr * update)
+                else:
+                    p.data.add_(-self.lr * self.adam.get_update(update, p.data, idx))
+        return weights
---
+++ renaissance/utils.py
@@ -0,0 +1,251 @@
+import torch
+import torch.nn as nn
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+import pickle as pkl
+import numpy as np
+from pathlib import Path 
+import copy 
+import random
+
+
+class MultiClassHingeLoss(nn.Module):
+    def __init__(self, reduction="mean"):
+        super(MultiClassHingeLoss, self).__init__()
+        self.reduction = reduction
+
+    def forward(self, output, y):
+        index = torch.arange(0, y.size()[0]).long()
+        output_y = output[index, y.data].view(-1,1)
+        loss = output - output_y + 1.0 
+        loss[index, y.data] = 0
+        loss[loss < 0] = 0
+        if self.reduction == "mean":
+            loss = torch.sum(loss, dim=1) / output.size()[1]
+
+        return loss 
+    
+def train(model, optimizer, weight_calculator, loss_fn, train_dataloader, test_dataloader, epochs, alg, device):
+
+    train_loss = []
+    test_acc = []
+
+    # Keep track of the classification and regression losses for plotting later.
+    model = model.to(device)
+    print(f"Using device: {device}")
+        
+    for epoch in range(epochs):
+        model.train()
+        # Keeping track of running losses.
+        class_running_loss = 0.
+        for i, data in enumerate(train_dataloader):
+            
+            # Get the inputs and the labels, put them on cuda
+            inputs, label_gts = data
+            inputs = inputs.to(device)
+            label_gts = label_gts.to(device)
+            
+            # Get the outputs from the model
+            label_pred = model(inputs)
+            sample_loss = loss_fn(label_pred, label_gts)
+            mean_loss = torch.mean(sample_loss)
+            optimizer.zero_grad()
+            mean_loss.backward()
+
+            if alg != 'sgd':
+                weight_calculator.sample_losses = sample_loss
+                weights = weight_calculator.calc_weights()
+                label_pred = model(inputs)
+                sample_loss = loss_fn(label_pred, label_gts)
+                new_loss = torch.sum(weights * sample_loss)
+                optimizer.zero_grad()
+                new_loss.backward()
+
+            optimizer.step()
+            # scheduler.step()
+            
+            # Update the running losses.
+            class_running_loss += torch.mean(sample_loss).item()
+    
+            # Just some user feedback reporting the losses.
+            if (i+1) % 50 == 0:
+                mean_loss = class_running_loss / 50
+                if (i+1) % 100 == 0:
+                    print("[epoch: %d, batch: %5d] class. loss: %.3f" % (epoch+1, i+1, mean_loss))
+                class_running_loss = 0.0
+                train_loss.append(mean_loss)
+        
+        with torch.no_grad():
+            model.eval()
+            total_class_loss = 0
+            correct = 0
+            total = 0
+            # Exactly the same stuff we do in training, just on the validation data and with no training, only
+            # evaluation!
+            for i, data in enumerate(test_dataloader):
+                inputs, label_gts = data
+                inputs = inputs.to(device)
+                label_gts = label_gts.to(device)
+                label_pred = model(inputs)
+                total_class_loss += torch.mean(loss_fn(label_pred, label_gts)).item()
+                
+                # Check the Accuracy of the model 
+                _, label_pred = torch.max(label_pred, dim=1)
+                # Update the tallies for total examples considered and correctly classified
+                correct += torch.eq(label_gts, label_pred).sum()
+                total += label_gts.shape[0]
+            
+            # At the end, calculate validation accuracy
+            accuracy = correct / total
+            test_acc.append(accuracy.item())
+            print("test accuracy", accuracy)
+            print("test loss", total_class_loss / len(test_dataloader))
+
+    return train_loss, test_acc
+
+
+def plot_loss(fig, save_path, plot_path, seeds, alg):
+    loss = 0
+    loss_arr = []
+    for seed in seeds:
+        full_name = f"{save_path}/loss_{alg}_{seed}_0.001.pkl"
+
+        with open(full_name, "rb") as f:
+            loss = np.array(pkl.load(f))
+            loss_arr.append(loss)
+
+    std = np.std(loss_arr, axis=0)
+    mean = np.mean(loss_arr, axis=0)
+    lower = mean - std
+    upper = mean + std
+
+    plt.fill_between(upper, lower, alpha=.2, linewidth=0)
+    fig.plot(mean)
+    return fig 
+    #plt.title("Training loss vs epochs")
+    #plt.grid("major")
+    #plt.savefig(f"{plot_path}/loss_{alg}.png", dpi=400, bbox_inches="tight")
+    #plt.close()
+
+
+def plot_acc(fig, save_path, plot_path, seeds, alg):
+    acc = 0
+    acc_arr = []
+    for seed in seeds:
+        full_name = f"{save_path}/acc_{alg}_{seed}_0.001.pkl"
+
+        with open(full_name, "rb") as f:
+            acc = np.array(pkl.load(f))
+            acc_arr.append(acc)
+
+    std = np.std(acc_arr, axis=0)
+    mean = np.mean(acc_arr, axis=0)
+    lower = mean - std
+    upper = mean + std
+
+    fig.fill_between(upper, lower, alpha=.2, linewidth=0)
+    fig.plot(mean)
+    #plt.title("Test accuracy vs epochs")
+    #plt.grid("major")
+    #plt.savefig(f"{plot_path}/acc_{alg}.png", dpi=400, bbox_inches="tight")
+    return fig 
+
+def make_noisy_data(train_data, noise_type, noise_frac, shift=1):
+    '''
+    This function takes a Dataset and make it noisy Dataset
+    by swapping labels either randomly or deterministically.
+    '''
+    # get dataset parameters 
+    data_size = len(train_data)
+    dataloader = torch.utils.data.DataLoader(train_data, batch_size=data_size, shuffle=True)
+    (data, target) = next(iter(dataloader))
+    class_labels = sorted(list(torch.unique(target)))
+    num_classes = len(class_labels)
+    # make labels noisy 
+    flag = np.random.binomial(1, noise_frac, size=(data_size, 1))
+    target_noisy = copy.deepcopy(target.numpy())
+    for idx, val in enumerate(flag):
+        class_idx = class_labels.index(target[idx].item())
+        if val[0] == 1 and noise_type == "directed":
+            out = class_labels[(class_idx + shift) % num_classes]
+        elif val[0] == 1 and noise_type == "random":
+            out = random.choice(class_labels[:class_idx] + class_labels[min(len(class_labels), class_idx+1):])
+        else:
+            continue 
+        target_noisy[idx] = out
+    target_noisy = torch.from_numpy(target_noisy)
+    print("Label shape:", target_noisy.shape)
+    print("Data shape:", data.shape)
+    print("Class labels:", class_labels)
+    noisy_data = torch.utils.data.TensorDataset(data, target_noisy)
+    return noisy_data
+
+if __name__ == '__main__':
+
+    save_path = Path("./results")
+    plot_path = Path("./plots")
+    seeds = [44,45,46] #47, 48
+
+    plt.figure()
+    plt.title("Training loss vs epochs (FMNIST, LeNet)")
+    plt.grid("major")
+    plt.xlabel("epochs")
+    plt.ylabel("cross-entropy loss")
+    alg_list = ["maxnorm_topk", "maxnorm_soft", "maxnorm_hard", "maxloss_topk",
+                "maxloss_soft", "maxloss_hard", "maxcorr_topk", "maxcorr_hard",
+                "klsgd_r", "sgd"]
+    for alg in alg_list:
+        loss = 0
+        loss_arr = []
+        for seed in seeds:
+            full_name = f"{save_path}/loss_{alg}_{seed}_0.001.pkl"
+
+            with open(full_name, "rb") as f:
+                loss = np.array(pkl.load(f))
+                loss_arr.append(loss)
+
+        std = np.std(loss_arr, axis=0)
+        mean = np.mean(loss_arr, axis=0)
+        lower = mean - std
+        upper = mean + std
+
+        #plt.fill_between(upper, lower, alpha=.2, linewidth=0)
+        plt.plot(mean)
+
+    plt.legend(alg_list)
+    plt.savefig(f"{plot_path}/loss.png", dpi=400, bbox_inches="tight")
+    plt.close()
+
+
+    # ACCURACY
+    plt.figure()
+    plt.title("Test accuracy vs epochs (FMNIST, LeNet)")
+    plt.grid("major")
+    plt.xlabel("epochs")
+    plt.ylabel("classification accuracy %")
+    alg_list = ["maxnorm_topk", "maxnorm_soft", "maxnorm_hard", "maxloss_topk",
+                "maxloss_soft", "maxloss_hard", "maxcorr_topk", "maxcorr_hard",
+                "klsgd_r", "sgd"]
+    for alg in alg_list:
+        loss = 0
+        loss_arr = []
+        for seed in seeds:
+            full_name = f"{save_path}/acc_{alg}_{seed}_0.001.pkl"
+
+            with open(full_name, "rb") as f:
+                loss = np.array(pkl.load(f))
+                loss_arr.append(loss)
+
+        std = np.std(loss_arr, axis=0)
+        mean = np.mean(loss_arr, axis=0)
+        lower = mean - std
+        upper = mean + std
+
+        #plt.fill_between(upper, lower, alpha=.2, linewidth=0)
+        plt.plot(mean)
+
+    plt.legend(alg_list)
+    plt.savefig(f"{plot_path}/acc.png", dpi=400, bbox_inches="tight")
+    plt.close()
+
+
