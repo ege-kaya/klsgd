828e12e735de31f7fad6dd5feac628f7849ad273
ege-kaya <43046947+ege-kaya@users.noreply.github.com>
2024-05-07 22:03:43 -0400

    Add files via upload

---
+++ reform/main.py
@@ -0,0 +1,296 @@
+import torch
+import torchvision
+import torchvision.transforms as tvt
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+import matplotlib.pyplot as plt
+from torch.utils.data import DataLoader
+import random
+import pickle as pkl
+import argparse
+from opacus.grad_sample import GradSampleModule
+from opacus.validators.module_validator import ModuleValidator
+from networks import Linear, LeNet, PreActResNet18
+from optimizers import KLSGD, WeightCalculator
+from utils import MultiClassHingeLoss, train, make_noisy_data
+from torch.utils.data.sampler import SubsetRandomSampler
+from torch.optim import lr_scheduler
+import os
+
+argparser = argparse.ArgumentParser()
+
+'''
+maxloss_hard: 4     minloss_hard: 5
+maxloss_soft: 6     maxnorm_soft: 7
+minloss_soft: 8     minnorm_soft: 9
+maxnorm_hard: 10    minnorm_hard: 11
+maxcorr_hard: 12    mincorr_hard: 13
+maxcorr_topk: 14    mincorr_topk: 15
+maxnorm_topk: 16    minnorm_topk: 17
+maxloss_topk: 18    minloss_topk: 19
+'''
+
+argparser.add_argument("--optimizer", default="sgd", type=str, choices=["sgd", "adam", "poscorr_soft",
+                                                              "maxcorr_soft", "mincorr_soft", 
+                                                              "maxloss_hard", "minloss_hard",
+                                                              "maxloss_soft", "maxnorm_soft",
+                                                              "minloss_soft", "minnorm_soft",
+                                                              "maxnorm_hard", "minnorm_hard",
+                                                              "maxcorr_hard", "mincorr_hard",
+                                                              "maxcorr_topk", "mincorr_topk",
+                                                              "maxnorm_topk", "minnorm_topk",
+                                                              "maxloss_topk", "minloss_topk"],
+                                                              help="optimizer to use")
+argparser.add_argument("--reg", default=1e-3, type=float, help="regularizer for KL term")
+argparser.add_argument("--lr", default=1e-3, type=float, help="learning rate") # 1e-3
+argparser.add_argument("--lr_decay", default=0.2, type=float, help="learning rate decay") # 0.2 
+argparser.add_argument("--weight_decay", default=1e-4, type=float, help="weight decay parameter")
+argparser.add_argument("--momentum", default=0.9, type=float, help="momentum parameter")
+argparser.add_argument("--decay_schedule", default=30, type=int, help="decay schedule") # 10? 
+argparser.add_argument("--epochs", default=50, type=int, help="number of epochs for training")
+argparser.add_argument("--device_idx", default=0, type=int, help="cuda device idx")
+argparser.add_argument("--batch_size", default=64, type=int, help="mini-batch size for training") # 64
+argparser.add_argument("--num_workers", default=4, type=int, help="number of workers on cpu for dataloaders")
+# argparser.add_argument("--seed", default=44, type=int, help="random seed")
+argparser.add_argument("--dataset", default="MNIST", type=str, choices=["semeion", "MNIST",
+                                                                        "KMNIST", "FMNIST",
+                                                                        "CIFAR10", "CIFAR100",
+                                                                        "SVHN"],
+                                                                        help="dataset to train on")
+argparser.add_argument("--model", default="LeNet", type=str, choices=["LeNet", "logistic", 
+                                                                      "SVM", "ResNet18"])
+argparser.add_argument("--aug", default=False, action='store_true', help="whether to augment data")
+argparser.add_argument("--topk_ratio", default=1., type=float, help="ratio of the top-k elements chosen from the batch")
+
+# file managment options 
+argparser.add_argument("--savepath", default="./results", type=str, help="directory for the loss/accuracy history")
+
+# noisy data settings 
+argparser.add_argument("--noisy_data", default=False, action='store_true', help="whether to generate noisy data")
+argparser.add_argument("--noise_frac", default=0.1, type=float, help="fraction of the noisy data")
+argparser.add_argument("--noise_type", default="random", type=str, choices=("shift", "random"), help="type of the noise (i.e. shift or random)")
+argparser.add_argument("--adaptive", default=False, action='store_true', help="whether to use adaptive topk ratio")
+argparser.add_argument('--seeds', nargs="+", type=int, default=[44, 45, 46, 47, 48])
+
+args = argparser.parse_args()
+
+device = f'cuda:{args.device_idx}' if torch.cuda.is_available() else 'cpu'
+
+optimizer = args.optimizer
+lr = args.lr
+reg = args.reg
+topk_ratio = args.topk_ratio
+data_path = "./data"
+train_transform = tvt.Compose([tvt.ToTensor()])
+test_transform = tvt.Compose([tvt.ToTensor()])
+train_Sampler = None
+test_Sampler = None
+Shuffle = True
+epochs = args.epochs
+save_path = args.savepath #"./results"
+#plot_path = "./plots"
+seeds = args.seeds
+
+os.makedirs(data_path, exist_ok=True)
+os.makedirs(save_path, exist_ok=True)
+#os.makedirs(plot_path, exist_ok=True)
+
+
+'''
+DATASET SELECTION
+'''
+
+if args.dataset == 'MNIST':
+    nh = 28
+    nw = 28
+    nc = 1
+    num_class = 10
+    end_epoch = 50
+    if args.aug:        
+        end_epoch = 200 
+        train_transform = tvt.Compose([
+                            tvt.RandomCrop(28, padding=2),
+                            tvt.RandomAffine(15, scale=(0.85, 1.15)),
+                            tvt.ToTensor()       
+                       ])                
+    train_data = torchvision.datasets.MNIST(data_path, train=True, download=True, transform=train_transform)
+    test_data = torchvision.datasets.MNIST(data_path, train=False, download=True, transform=test_transform)
+elif args.dataset == 'CIFAR10':
+    nh = 32
+    nw = 32
+    nc = 3
+    num_class = 10 
+    end_epoch = 50
+    if args.aug:
+        end_epoch = 200 
+        train_transform = tvt.Compose([
+            tvt.RandomCrop(32, padding=4),
+            tvt.RandomHorizontalFlip(),
+            tvt.ToTensor(),
+            tvt.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+        ])
+        test_transform = tvt.Compose([
+            tvt.ToTensor(),
+            tvt.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+        ])
+    train_data = torchvision.datasets.CIFAR10(root=data_path, train=True, download=True, transform=train_transform)
+    test_data = torchvision.datasets.CIFAR10(root=data_path, train=False, download=True, transform=test_transform)
+elif args.dataset == 'CIFAR100':
+    nh = 32
+    nw = 32
+    nc = 3
+    num_class = 100
+    end_epoch = 50
+    if args.aug:
+        end_epoch = 200    
+        train_transform = tvt.Compose([
+            tvt.RandomCrop(32, padding=4),
+            tvt.RandomHorizontalFlip(),
+            tvt.ToTensor()
+        ])
+        test_transform = tvt.Compose([
+            tvt.ToTensor()
+        ])
+    train_data = torchvision.datasets.CIFAR100(root=data_path, train=True, download=True, transform=train_transform)
+    test_data = torchvision.datasets.CIFAR100(root=data_path, train=False, download=True, transform=test_transform)
+elif args.dataset == 'SVHN':
+    nh = 32
+    nw = 32
+    nc = 3
+    num_class = 10
+    end_epoch = 50    
+    if args.aug:
+        end_epoch = 200
+        train_transform = tvt.Compose([
+            tvt.RandomCrop(32, padding=4),
+            tvt.RandomHorizontalFlip(),
+            tvt.ToTensor()
+        ])
+        test_transform = tvt.Compose([
+            tvt.ToTensor()
+        ])
+    train_data = torchvision.datasets.SVHN(data_path, split='train', download=True, transform=train_transform)
+    test_data = torchvision.datasets.SVHN(data_path, split='test', download=True, transform=test_transform)    
+elif args.dataset == 'FMNIST':
+    nh = 28
+    nw = 28
+    nc = 1
+    num_class = 10
+    end_epoch = 20
+    if args.aug:
+        end_epoch = 200       
+        train_transform = tvt.Compose([
+            tvt.RandomCrop(28, padding=2),
+            tvt.RandomHorizontalFlip(),
+            tvt.ToTensor()
+        ]) 
+    train_data = torchvision.datasets.FashionMNIST(data_path, train=True, download=True, transform=train_transform)
+    test_data = torchvision.datasets.FashionMNIST(data_path, train=False, download=True, transform=test_transform)
+elif args.dataset == 'KMNIST':
+    nh = 28
+    nw = 28
+    nc = 1
+    num_class = 10
+    end_epoch = 50
+    if args.aug:
+        end_epoch = 200
+        train_transform = tvt.Compose([
+                            tvt.RandomCrop(28, padding=2),
+                            tvt.ToTensor()       
+                       ])
+    train_data = torchvision.datasets.KMNIST(data_path, train=True, download=True, transform=train_transform)
+    test_data = torchvision.datasets.KMNIST(data_path, train=False, download=True, transform=test_transform)
+elif args.dataset == 'semeion':
+    nh = 16
+    nw = 16
+    nc = 1
+    num_class = 10 # the digits from 0 to 9 (written by 80 people twice)    
+    end_epoch = 50
+    if args.aug:
+        end_epoch = 200
+        train_transform = tvt.Compose([
+            tvt.RandomCrop(16, padding=1),
+            tvt.RandomAffine(4, scale=(1.05, 1.05)),
+            tvt.ToTensor()
+        ])
+        test_transform = tvt.Compose([
+            tvt.ToTensor()
+        ])    
+    train_data = torchvision.datasets.SEMEION(data_path, transform=train_transform, download=True) 
+    test_data = train_data    
+    random_index = np.load(data_path+'/random_index.npy')
+    train_size = 1000    
+    train_Sampler = SubsetRandomSampler(random_index[range(train_size)])
+    test_Sampler = SubsetRandomSampler(random_index[range(train_size,len(test_data))])
+    Shuffle = False
+else:
+    raise ValueError(f"Invalid dataset: {args.dataset}")
+
+model = args.model
+
+
+'''
+MODEL SELECTION
+'''
+
+def get_model(model_name):
+    model = None
+    if model_name == 'LeNet':
+        model = LeNet(nc, nh, nw, num_class).to(device)
+    elif model_name == 'ResNet18':
+        model = PreActResNet18(nc, num_class).to(device)
+    elif model_name == 'logistic' or args.model == 'SVM':
+        dx = nh * nw * nc     
+        model = Linear(dx, num_class).to(device)
+    else:
+        raise ValueError(f"Invalid model: {args.model}")
+    
+    return ModuleValidator.fix(model)
+
+'''
+LOSS SELECTION
+'''
+
+if args.model == "SVM":
+    loss = MultiClassHingeLoss(reduction="none")
+else:
+    loss = nn.CrossEntropyLoss(reduction='none')
+
+
+
+if __name__ == "__main__":
+    for seed in seeds:
+        random.seed(seed)
+        torch.manual_seed(seed)
+        torch.cuda.manual_seed(seed)
+        np.random.seed(seed)
+        torch.backends.cudnn.deterministic=True
+        torch.backends.cudnn.benchmarks=False
+        torch.autograd.set_detect_anomaly(True)
+
+        
+        # generate noisy data if wanted
+        train_data, _ = torch.utils.data.random_split(make_noisy_data(train_data, args.noise_type, args.noise_frac) if args.noisy_data else train_data, [5000, 55000])
+
+        train_loader = DataLoader(train_data, batch_size=args.batch_size, sampler=train_Sampler, shuffle=Shuffle, drop_last=True)
+        test_loader = DataLoader(test_data, batch_size=args.batch_size, sampler=test_Sampler, shuffle=False)
+
+        model = get_model(args.model)
+        if not args.optimizer == "sgd" and not args.optimizer == "adam":
+            model = GradSampleModule(model)
+                
+        #optimizer = get_optimizer(args.optimizer, model)
+        weight_calculator = WeightCalculator(params=model.parameters(), alg_name=args.optimizer, topk_ratio=args.topk_ratio, reg=args.reg)
+        optimizer = torch.optim.SGD(params=model.parameters(), lr=lr, weight_decay=args.weight_decay, momentum=args.momentum)
+        # scheduler = lr_scheduler.StepLR(optimizer, step_size=args.decay_sch
+        losses, accs = train(model, optimizer, weight_calculator, loss, train_loader, test_loader, epochs, args.optimizer, device, args.adaptive)
+
+        with open(f"{save_path}/acc_{args.optimizer}_{seed}_{lr}.pkl", "wb") as f:
+            pkl.dump(accs, f)
+        
+        with open(f"{save_path}/loss_{args.optimizer}_{seed}_{lr}.pkl", "wb") as f:
+            pkl.dump(losses, f)
+
+
+
---
+++ reform/utils.py
@@ -0,0 +1,326 @@
+import torch
+import torch.nn as nn
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+import pickle as pkl
+import numpy as np
+from pathlib import Path 
+import copy 
+import random
+import torch.nn.functional as F 
+
+
+class MultiClassHingeLoss(nn.Module):
+    def __init__(self, reduction="mean"):
+        super(MultiClassHingeLoss, self).__init__()
+        self.reduction = reduction
+
+    def forward(self, output, y):
+        index = torch.arange(0, y.size()[0]).long()
+        output_y = output[index, y.data].view(-1,1)
+        loss = output - output_y + 1.0 
+        loss[index, y.data] = 0
+        loss[loss < 0] = 0
+        if self.reduction == "mean":
+            loss = torch.sum(loss, dim=1) / output.size()[1]
+
+        return loss 
+
+def output(data_loader, model, alg, device, train=False):
+    if train is True:
+        model.train()
+    elif train is False:
+        model.eval()
+    total_loss = 0    
+    total_correct = 0      
+    total_size = 0   
+    for batch_idx, (x, y) in enumerate(data_loader):
+        x, y = x.to(device), y.to(device)
+        h1 = model(x)
+        y_hat = h1.max(1)[1]
+        if alg == 'SVM':
+            total_loss += torch.mean(hinge_loss(h1, y)).item() * y.size(0)
+        else:                
+            total_loss += F.cross_entropy(h1, y).item() * y.size(0)
+        total_correct += y_hat.eq(y.data).cpu().sum()                
+        total_size += y.size(0)    
+    # print
+    total_loss /= total_size 
+    total_acc = 100. * float(total_correct) / float(total_size)  
+    return (total_loss, total_acc)  
+    
+def train(model, optimizer, weight_calculator, loss_fn, train_dataloader, test_dataloader, epochs, alg, device, adaptive):
+
+    train_loss, train_acc, test_loss, test_acc = [],[0.],[],[0.]
+    init_topk_ratio = weight_calculator.topk_ratio
+    # Keep track of the classification and regression losses for plotting later.
+    model = model.to(device)
+    print(f"Using device: {device}")
+
+    for epoch in range(epochs):
+        if adaptive:
+            if test_acc[-1] >= 99.5:
+                weight_calculator.topk_ratio = init_topk_ratio / 16
+            elif test_acc[-1] >= 95.:
+                weight_calculator.topk_ratio = init_topk_ratio / 8
+            elif test_acc[-1] >= 90.:
+                weight_calculator.topk_ratio = init_topk_ratio / 4
+            elif test_acc[-1] >= 80.:
+                weight_calculator.topk_ratio = init_topk_ratio / 2
+
+        model.train()
+        # Keeping track of running losses.
+        class_running_loss = 0.
+        for i, data in enumerate(train_dataloader):
+            
+            # Get the inputs and the labels, put them on cuda
+            inputs, label_gts = data
+            inputs = inputs.to(device)
+            label_gts = label_gts.to(device)
+            
+            # Get the outputs from the model
+            optimizer.zero_grad()
+            label_pred = model(inputs)
+            sample_loss = loss_fn(label_pred, label_gts)
+            mean_loss = torch.mean(sample_loss)
+            mean_loss.backward()
+
+            if alg != 'sgd':
+                weight_calculator.sample_losses = sample_loss.detach()
+                weights = weight_calculator.calc_weights()
+                optimizer.zero_grad()
+                label_pred = model(inputs)
+                sample_loss2 = loss_fn(label_pred, label_gts)
+                new_loss = torch.sum(weights * sample_loss2)
+                new_loss.backward()
+
+            optimizer.step()
+            # scheduler.step()
+            
+            # Update the running losses.
+            class_running_loss += torch.mean(sample_loss).item()
+    
+            # Just some user feedback reporting the losses.
+            if (i+1) % 50 == 0:
+                mean_loss = class_running_loss / 50
+                if (i+1) % 100 == 0:
+                    print("[epoch: %d, batch: %5d] class. loss: %.3f" % (epoch+1, i+1, mean_loss))
+                class_running_loss = 0.0
+                #train_loss.append(mean_loss)
+        optimizer = lr_scheduler(optimizer, epoch + 1)
+        # evaluate the loss & accuracy of the model over train & test datasets 
+        (tr_loss, tr_acc) = output(train_dataloader, model, alg, device, train=True)
+        (te_loss, te_acc) = output(test_dataloader, model, alg, device, train=False)
+        # save the losses & accuracies 
+        train_loss.append(tr_loss)
+        train_acc.append(tr_acc)
+        test_loss.append(te_loss)
+        test_acc.append(te_acc)
+
+        # display the values 
+        print("*"*20)
+        print(f"train accuracy: {tr_acc}")
+        print(f"test accuracy: {te_acc}")
+        print("*"*20)
+    
+        # update the top-k ratio
+        if tr_acc >= 99.5:
+            weight_calculator.topk_ratio = 0.0625
+            optimizer = lr_decay_func(optimizer, lr_decay=0.5)
+        elif tr_acc >= 95.0:
+            weight_calculator.topk_ratio = 0.125
+        elif tr_acc >= 90.0:
+            weight_calculator.topk_ratio = 0.25
+        elif tr_acc >= 80.0:
+            weight_calculator.topk_ratio = 0.5
+
+
+
+    return train_loss, test_acc
+
+
+def plot_loss(fig, save_path, plot_path, seeds, alg):
+    loss = 0
+    loss_arr = []
+    for seed in seeds:
+        full_name = f"{save_path}/loss_{alg}_{seed}_0.001.pkl"
+
+        with open(full_name, "rb") as f:
+            loss = np.array(pkl.load(f))
+            loss_arr.append(loss)
+
+    std = np.std(loss_arr, axis=0)
+    mean = np.mean(loss_arr, axis=0)
+    lower = mean - std
+    upper = mean + std
+
+    plt.fill_between(upper, lower, alpha=.2, linewidth=0)
+    fig.plot(mean)
+    return fig 
+    #plt.title("Training loss vs epochs")
+    #plt.grid("major")
+    #plt.savefig(f"{plot_path}/loss_{alg}.png", dpi=400, bbox_inches="tight")
+    #plt.close()
+
+
+def plot_acc(fig, save_path, plot_path, seeds, alg):
+    acc = 0
+    acc_arr = []
+    for seed in seeds:
+        full_name = f"{save_path}/acc_{alg}_{seed}_0.001.pkl"
+
+        with open(full_name, "rb") as f:
+            acc = np.array(pkl.load(f))
+            acc_arr.append(acc)
+
+    std = np.std(acc_arr, axis=0)
+    mean = np.mean(acc_arr, axis=0)
+    lower = mean - std
+    upper = mean + std
+
+    fig.fill_between(upper, lower, alpha=.2, linewidth=0)
+    fig.plot(mean)
+    #plt.title("Test accuracy vs epochs")
+    #plt.grid("major")
+    #plt.savefig(f"{plot_path}/acc_{alg}.png", dpi=400, bbox_inches="tight")
+    return fig 
+
+def make_noisy_data(train_data, noise_type, noise_frac, shift=1):
+    '''
+    This function takes a Dataset and make it noisy Dataset
+    by swapping labels either randomly or deterministically.
+    '''
+    # get dataset parameters 
+    data_size = len(train_data)
+    dataloader = torch.utils.data.DataLoader(train_data, batch_size=data_size, shuffle=True)
+    (data, target) = next(iter(dataloader))
+    class_labels = sorted(list(torch.unique(target)))
+    num_classes = len(class_labels)
+    # make labels noisy 
+    flag = np.random.binomial(1, noise_frac, size=(data_size, 1))
+    target_noisy = copy.deepcopy(target.numpy())
+    for idx, val in enumerate(flag):
+        class_idx = class_labels.index(target[idx].item())
+        if val[0] == 1 and noise_type == "directed":
+            out = class_labels[(class_idx + shift) % num_classes]
+        elif val[0] == 1 and noise_type == "random":
+            out = random.choice(class_labels[:class_idx] + class_labels[min(len(class_labels), class_idx+1):])
+        else:
+            continue 
+        target_noisy[idx] = out
+    target_noisy = torch.from_numpy(target_noisy)
+    print("Label shape:", target_noisy.shape)
+    print("Data shape:", data.shape)
+    print("Class labels:", class_labels)
+    noisy_data = torch.utils.data.TensorDataset(data, target_noisy)
+    return noisy_data
+
+if __name__ == '__main__':
+
+    save_path = Path("./results")
+    plot_path = Path("./plots")
+    seeds = [44,45,46] #47, 48
+
+    plt.figure()
+    plt.title("Training loss vs epochs (FMNIST, LeNet)")
+    plt.grid("major")
+    plt.xlabel("epochs")
+    plt.ylabel("cross-entropy loss")
+    alg_list = ["maxnorm_topk", "maxnorm_soft", "maxnorm_hard", "maxloss_topk",
+                "maxloss_soft", "maxloss_hard", "maxcorr_topk", "maxcorr_hard",
+                "klsgd_r", "sgd"]
+    for alg in alg_list:
+        loss = 0
+        loss_arr = []
+        for seed in seeds:
+            full_name = f"{save_path}/loss_{alg}_{seed}_0.001.pkl"
+
+            with open(full_name, "rb") as f:
+                loss = np.array(pkl.load(f))
+                loss_arr.append(loss)
+
+        std = np.std(loss_arr, axis=0)
+        mean = np.mean(loss_arr, axis=0)
+        lower = mean - std
+        upper = mean + std
+
+        #plt.fill_between(upper, lower, alpha=.2, linewidth=0)
+        plt.plot(mean)
+
+    plt.legend(alg_list)
+    plt.savefig(f"{plot_path}/loss.png", dpi=400, bbox_inches="tight")
+    plt.close()
+
+
+    # ACCURACY
+    plt.figure()
+    plt.title("Test accuracy vs epochs (FMNIST, LeNet)")
+    plt.grid("major")
+    plt.xlabel("epochs")
+    plt.ylabel("classification accuracy %")
+    alg_list = ["maxnorm_topk", "maxnorm_soft", "maxnorm_hard", "maxloss_topk",
+                "maxloss_soft", "maxloss_hard", "maxcorr_topk", "maxcorr_hard",
+                "klsgd_r", "sgd"]
+    for alg in alg_list:
+        loss = 0
+        loss_arr = []
+        for seed in seeds:
+            full_name = f"{save_path}/acc_{alg}_{seed}_0.001.pkl"
+
+            with open(full_name, "rb") as f:
+                loss = np.array(pkl.load(f))
+                loss_arr.append(loss)
+
+        std = np.std(loss_arr, axis=0)
+        mean = np.mean(loss_arr, axis=0)
+        lower = mean - std
+        upper = mean + std
+
+        #plt.fill_between(upper, lower, alpha=.2, linewidth=0)
+        plt.plot(mean)
+
+    plt.legend(alg_list)
+    plt.savefig(f"{plot_path}/acc.png", dpi=400, bbox_inches="tight")
+    plt.close()
+
+def lr_decay_func(optimizer, lr_decay=0.1):
+    for param_group in optimizer.param_groups:
+        param_group['lr'] *= lr_decay
+    return optimizer    
+def lr_scheduler(optimizer, epoch, lr_decay=0.1, interval=10):
+    #if args.data_aug == 0:
+    if epoch == 10 or epoch == 50:
+        optimizer = lr_decay_func(optimizer, lr_decay=lr_decay) 
+    #if args.data_aug == 1:
+    #    if epoch == 10 or epoch == 100:
+    #        optimizer = lr_decay_func(optimizer, lr_decay=lr_decay)                   
+    return optimizer
+
+
+'''
+        with torch.no_grad():
+            model.eval()
+            total_class_loss = 0
+            correct = 0
+            total = 0
+            # Exactly the same stuff we do in training, just on the validation data and with no training, only
+            # evaluation!
+            for i, data in enumerate(test_dataloader):
+                inputs, label_gts = data
+                inputs = inputs.to(device)
+                label_gts = label_gts.to(device)
+                label_pred = model(inputs)
+                total_class_loss += torch.mean(loss_fn(label_pred, label_gts)).item()
+                
+                # Check the Accuracy of the model 
+                _, label_pred = torch.max(label_pred, dim=1)
+                # Update the tallies for total examples considered and correctly classified
+                correct += torch.eq(label_gts, label_pred).sum()
+                total += label_gts.shape[0]
+            
+            # At the end, calculate validation accuracy
+            accuracy = correct / total
+            test_acc.append(accuracy.item())
+            print("test accuracy", accuracy)
+            print("test loss", total_class_loss / len(test_dataloader))
+        '''
